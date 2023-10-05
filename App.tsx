891593e1b9ba7c98985080da0d32263002a16cdf
Aly Lopes <alopes@findhotel.net>
2023-10-05 11:54:36 -0300

    feat: change page title

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import type { Route } from "@react-navigation/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "@shopify/restyle";
 
@@ -5,13 +6,29 @@ import AppStatusContext from "./src/AppStatusContext";
 import Application from "./src/Application";
 import SplashScreenProvider from "./src/SplashScreenProvider";
 import useStartApp from "./src/hooks/useStartApp";
+import ROUTES, { PAGE_TITLES } from "./src/routes";
 import { theme } from "./src/theme";
+
+function formatter(
+  options: Record<string, any> | undefined,
+  route: Route<string> | undefined,
+) {
+  if (options?.title) return options.title;
+
+  const routeName = (route?.name ?? ROUTES.Home) as keyof typeof ROUTES;
+
+  return PAGE_TITLES[routeName];
+}
 
 export default function App() {
   const contextValue = useStartApp();
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      documentTitle={{
+        formatter,
+      }}
+    >
       <ThemeProvider theme={theme}>
         <AppStatusContext.Provider value={contextValue}>
           <SplashScreenProvider>
--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,8 @@ const ROUTES = {
   Home: "Home",
 } as const;
 
+export const PAGE_TITLES = {
+  [ROUTES.Home]: "Aly Lopes | Code Maker | Tech Lead",
+};
+
 export default ROUTES;
