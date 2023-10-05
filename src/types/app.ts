b8eafbe02d676a3b615531ddd068f87e553a224e
Aly Lopes <alopes@findhotel.net>
2023-10-05 10:25:07 -0300

    feat: add Github and Linkedin icons

--- src/types/app.ts
+++ src/types/app.ts
@@ -2,3 +2,8 @@ import type { ValuesOf } from "./utils";
 import type { APP_STATUS } from "../constants";
 
 export type AppStatus = ValuesOf<typeof APP_STATUS>;
+
+export type IconProps = {
+  size: number;
+  color?: string;
+};
